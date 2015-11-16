653afd7075efe878c97bfe3be6e58e7a716381f9
Dayany Espindola <dayanyrec@gmail.com>
2015-11-16 11:42:58 -0200

    Multiplicação de veotr por escalar

--- ublas-vector/main.cpp
+++ ublas-vector/main.cpp
@@ -11,5 +11,9 @@ int main () {
     for (i = 0; i < v.size (); ++ i)
         v (i) = i;
 
+    //multiplicacao por escalar
+    vector<double> w = 3.0 * v;
+
     std::cout << "v=\n" << v << std::endl;
+    std::cout << "w=\n" << w << std::endl;
 }
